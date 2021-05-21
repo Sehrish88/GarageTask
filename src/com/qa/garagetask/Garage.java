467e6e52616a534552c29ad3a7837d6e195ab49a
Sehrish88 <sehrishbalouch4@gmail.com>
2021-05-21 11:08:09 -0400

    add attributes to classes

--- src/com/qa/garagetask/Garage.java
+++ src/com/qa/garagetask/Garage.java
@@ -1,5 +1,12 @@
 package com.qa.garagetask;
 
-public class Garage {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Garage {
+	protected List<Vehicle> storage = new ArrayList<>();
+	public void addVehicle(Vehicle input){
+		storage.add(input); 
+		//remove method that accepts index or type use clear method to empty garage, implement bill method g
+	}
 }
