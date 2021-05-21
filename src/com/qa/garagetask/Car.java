66206d472a3170c647700774d9210d753d21a3b8
Sehrish88 <sehrishbalouch4@gmail.com>
2021-05-21 10:58:36 -0400

    add the classes and their constructors

--- src/com/qa/garagetask/Car.java
+++ src/com/qa/garagetask/Car.java
@@ -1,5 +1,12 @@
 package com.qa.garagetask;
 
-public class Car {
-
+public class Car extends Vehicle {
+	public boolean isElectric; 
+	public Car(String make, String model, int year, String color, boolean isElectric) {
+		super(make, model, year, color);
+		// TODO Auto-generated constructor stub
+		this.isElectric = isElectric; 
+		
+	}
+	
 }
