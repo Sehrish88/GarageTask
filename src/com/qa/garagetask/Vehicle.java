686824f314cb7dabc0cfe29a9dc5d3ef5511b5c8
Sehrish88 <sehrishbalouch4@gmail.com>
2021-05-21 09:09:47 -0400

    create a vehicle class and a constructor for it

--- src/com/qa/garagetask/Vehicle.java
+++ src/com/qa/garagetask/Vehicle.java
@@ -1,5 +1,19 @@
 package com.qa.garagetask;
 
 public class Vehicle {
-
+	public String make;
+	public String model;
+	public int year;
+	public String color;
+	
+	public Vehicle(String make, String model, int year, String color) {
+		this.make = make;
+		this.model = model;
+		this.year = year;
+		this.color = color;
+		
+	}
+	public static void main(String[] args) {
+		 
+	}
 }
