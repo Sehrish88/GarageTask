09cb2ffdd18d1e9b0bed7f8d395ddbe894dc399f
Sehrish88 <sehrishbalouch4@gmail.com>
2021-05-23 19:23:12 -0400

    Add methods to Garage class

--- src/com/qa/garagetask/Garage.java
+++ src/com/qa/garagetask/Garage.java
@@ -4,14 +4,47 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Garage {
-	protected List<Vehicle> storage = new ArrayList<>();
+	String garageName;
+	
+	public Garage(String garageName){
+		this.garageName = garageName; 
+	}
+	
+	
+	
+	protected List<Vehicle> storage = new ArrayList<Vehicle>();
+	
 	public void addVehicle(Vehicle input){
 		storage.add(input); 
-		//remove method that accepts index or type use clear method to empty garage, implement bill method 
-		
-		
 	}
+	
+
+	String getGarageName() {
+		return garageName;
+	}
+	
 	public void removeVehicle(int indexNum){
 		storage.remove(indexNum);
 	}
+	
+	public void emptyGarage() {
+		storage.clear();
+	}
+	
+	 public void vehicleServiceBill(Vehicle input) {
+		 double totalCost = 0;
+		 
+		 for (Vehicle v: storage) {
+			 if(v instanceof Car) {
+				 totalCost += 500;
+			 }
+			 else if(v instanceof Truck) {
+				 totalCost += 600;
+			 } else {
+				 totalCost += 300;
+			 }
+		 }
+		 
+		 System.out.println("Your total vehicle service bill is " + totalCost); 
+	 }
 }
