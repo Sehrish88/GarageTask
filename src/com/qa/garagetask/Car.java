139c1ffbf38172de0898686b673cf34bd7662774
Sehrish88 <sehrishbalouch4@gmail.com>
2021-05-23 19:22:30 -0400

    Add attributes and methods o Car subclass

--- src/com/qa/garagetask/Car.java
+++ src/com/qa/garagetask/Car.java
@@ -2,6 +2,7 @@ package com.qa.garagetask;
 
 public class Car extends Vehicle {
 	public boolean isElectric; 
+	
 	public Car(String make, String model, int year, String color, boolean isElectric) {
 		super(make, model, year, color);
 		// TODO Auto-generated constructor stub
@@ -11,22 +12,25 @@ public class Car extends Vehicle {
 	@Override
 	public void oilChange() {
 		// TODO Auto-generated method stub
-		
+		System.out.println("The service lights are blinking, time for an oil change!"); 
 	}
 	@Override
-	public void servic() {
+	public void service() {
 		// TODO Auto-generated method stub
+		System.out.println("The service lights are blinking, time to get your car serviced!"); 
 		
 	}
 	@Override
 	public void accelerate() {
 		// TODO Auto-generated method stub
+		System.out.println("Vroom vroom! The car is accelerating!"); 
 		
 	}
 	@Override
 	public void brake() {
 		// TODO Auto-generated method stub
+		System.out.println("You hit the brakes, the car is slowing down..."); 
 		
 	}
 	
-}
+} 
