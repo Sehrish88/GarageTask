39a73f088a41d4ae9f8b45e918641919807b2362
Sehrish88 <sehrishbalouch4@gmail.com>
2021-05-23 16:57:12 -0400

    Have Motorcycle extend the Vehicle class

--- src/com/qa/garagetask/Motorcycle.java
+++ src/com/qa/garagetask/Motorcycle.java
@@ -1,5 +1,35 @@
 package com.qa.garagetask;
 
-public class Motorcycle {
+public class Motorcycle extends Vehicle {
+
+	public Motorcycle(String make, String model, int year, String color) {
+		super(make, model, year, color);
+		// TODO Auto-generated constructor stub
+		
+	}
+
+	@Override
+	public void oilChange() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void servic() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void accelerate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void brake() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
