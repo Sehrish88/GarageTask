ff6bf220eec3df493d178211d7a329d41f245899
Sehrish88 <sehrishbalouch4@gmail.com>
2021-05-23 16:55:10 -0400

    create an ArrayList of vehicles of cars in the garage

--- src/com/qa/garagetask/Garage.java
+++ src/com/qa/garagetask/Garage.java
@@ -7,7 +7,8 @@ public class Garage {
 	protected List<Vehicle> storage = new ArrayList<>();
 	public void addVehicle(Vehicle input){
 		storage.add(input); 
-		//remove method that accepts index or type use clear method to empty garage, implement bill method g
+		//remove method that accepts index or type use clear method to empty garage, implement bill method 
+		
 		
 	}
 	public void removeVehicle(int indexNum){
