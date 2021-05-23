b03957021da295bbc835226bf854c53f69b00552
Sehrish88 <sehrishbalouch4@gmail.com>
2021-05-23 19:24:02 -0400

    add methods and attribute to Motorcycle class

--- src/com/qa/garagetask/Motorcycle.java
+++ src/com/qa/garagetask/Motorcycle.java
@@ -1,35 +1,50 @@
 package com.qa.garagetask;
 
 public class Motorcycle extends Vehicle {
+	public String type;
 
-	public Motorcycle(String make, String model, int year, String color) {
+	public Motorcycle(String make, String model, int year, String color, String type) {
 		super(make, model, year, color);
 		// TODO Auto-generated constructor stub
+		this.type = type; 
+		
+	}
+	
+	public void setType(String type) {
+		if(type.equals("standard") || type.equals("cruiser") || type.equals("sport bike") || type.equals("scooter") || type.equals("moped")) {
+			this.type = type;
+		} else {
+			this.type = "standard"; 
+		}
 		
 	}
 
 	@Override
 	public void oilChange() {
 		// TODO Auto-generated method stub
+		System.out.println("Your motorcycle is due for an oil change!"); 
 		
 	}
 
 	@Override
-	public void servic() {
+	public void service() {
 		// TODO Auto-generated method stub
+		
+		System.out.println("Your motorcycle is due for servicing"); 
 		
 	}
 
 	@Override
 	public void accelerate() {
 		// TODO Auto-generated method stub
+		System.out.println("Vroooom! Your motor cycle is accelerating!");
 		
 	}
 
 	@Override
 	public void brake() {
 		// TODO Auto-generated method stub
-		
+		System.out.println("Your motorcycle is braking and slowing down!"); 
 	}
 
 }
