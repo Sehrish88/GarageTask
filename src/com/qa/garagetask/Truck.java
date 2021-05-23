a22018cbfa796e4a9d68e09c3a194fd276f44b1d
Sehrish88 <sehrishbalouch4@gmail.com>
2021-05-23 16:59:44 -0400

    Have Trruck extend Vehicle

--- src/com/qa/garagetask/Truck.java
+++ src/com/qa/garagetask/Truck.java
@@ -1,5 +1,34 @@
 package com.qa.garagetask;
 
-public class Truck {
+public class Truck extends Vehicle{
+
+	public Truck(String make, String model, int year, String color) {
+		super(make, model, year, color);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void oilChange() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void servic() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void accelerate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void brake() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
